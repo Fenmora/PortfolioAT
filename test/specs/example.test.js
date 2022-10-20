e76e7006789dd1fc800e2ba232f22b15553d3872
fmora <fmora@cmnhospitals.org>
2022-10-20 08:53:48 -0600

    validate redirection of url

--- test/specs/example.test.js
+++ test/specs/example.test.js
@@ -26,48 +26,109 @@ describe("Home Page", () => {
   it("Validate Title & Subtitle", async () => {
     expect(await LoginPage.title.getText()).toEqual("Hi ! I'm Alex");
     expect(await LoginPage.subTitle.getText()).toEqual("Frontend developer focused on take awesome designs to the Web.");
+    //  Alter text
   });
 
-  it("Validate list contact elements", async () => {
+  it("Validate Check my work - Github", async () => {
     const formHeaders = await LoginPage.navbarContact;
-    expect(formHeaders.length).toEqual(3);
-    formHeaders[0].scrollIntoView();
     expect(await formHeaders[0].getText()).toEqual("Check my work");
-    formHeaders[1].scrollIntoView();
-    expect(await formHeaders[1].getText()).toEqual("Let's connect");
-    formHeaders[2].scrollIntoView();
-    expect(await formHeaders[2].getText()).toEqual("Contribute to community");
-  });
 
-  it("Validate list contact elements redirection ", async () => {
-    const formHeaders = await LoginPage.navbarContact;
     await formHeaders[0].click();
-    // await browser.pause(2000);
+
+    const windowHandles = await browser.getWindowHandles();
+    await browser.switchToWindow(windowHandles[1]);
+    expect(await browser.getUrl()).toEqual("https://github.com/alexcu21");
+    await browser.switchToWindow(windowHandles[0]);
+  });
+
+  it("Validate Let's connect - Linkedin", async () => {
+    const formHeaders = await LoginPage.navbarContact;
+    expect(await formHeaders[1].getText()).toEqual("Let's connect");
     await formHeaders[1].click();
-    // await browser.pause(2000);
+
+    const windowHandles = await browser.getWindowHandles();
+    await browser.switchToWindow(windowHandles[2]);
+    expect(await browser.getUrl()).toEqual("https://www.linkedin.com/in/alex-cuadra/");
+    await browser.switchToWindow(windowHandles[0]);
+  });
+
+  it("Contribute to community - Wp Article", async () => {
+    const formHeaders = await LoginPage.navbarContact;
+    expect(await formHeaders[2].getText()).toEqual("Contribute to community");
+
     await formHeaders[2].click();
-    // await browser.pause(2000);
+    const windowHandles = await browser.getWindowHandles();
+    await browser.switchToWindow(windowHandles[3]);
+    expect(await browser.getUrl()).toEqual("https://profiles.wordpress.org/alexcu21/profile/");
+    await browser.switchToWindow(windowHandles[0]);
   });
 
-  it("Validate list latest Projects", async () => {
-    const cardProject = await LoginPage.latestProjectList.$$(" div.css-1lda7xt.eqzxucj5 > article");
-    expect(cardProject.length).toEqual(3);
+  // it("Validate list contact elements", async () => {
+  //   const formHeaders = await LoginPage.navbarContact;
+  //   expect(formHeaders.length).toEqual(3);
+  //   // formHeaders[0].scrollIntoView();
+  //   expect(await formHeaders[0].getText()).toEqual("Check my work");
+  //   // formHeaders[1].scrollIntoView();
+  //   expect(await formHeaders[1].getText()).toEqual("Let's connect");
+  //   // formHeaders[2].scrollIntoView();
+  //   expect(await formHeaders[2].getText()).toEqual("Contribute to community");
+  // });
 
-    console.log("*****");
-    console.log(LoginPage.latestProjectListOptions[0].getText());
-    expect(await LoginPage.latestProjectListOptions.isDisplayed()).toBeTruthy();
+  // it("Validate list contact elements redirection ", async () => {
+  //   const formHeaders = await LoginPage.navbarContact;
+  //   await formHeaders[0].click();
+  //   await browser.pause(2000);
+  //   await formHeaders[1].click();
+  //   await browser.pause(2000);
+  //   await formHeaders[2].click();
+  //   await browser.pause(2000);
 
-    await loginPage.firstProject.click();
-    // await browser.pause(5000);
-    await loginPage.firstProjectCode.click();
-    // await browser.pause(5000);
-    await loginPage.secondProject.click();
-    // await browser.pause(5000);
-    await loginPage.secondProjectCode.click();
-    // await browser.pause(5000);
-    await loginPage.thirdProject.click();
-    // await browser.pause(5000);
-    await loginPage.thirdProjectCode.click();
-    // await browser.pause(5000);
-  });
+  //   //dividirlo 1. por 1. validar texto. click compare browser get url equal
+  // });
+
+  // it("Validate list latest Projects", async () => {
+  //   const cardProject = await LoginPage.latestProjectList.$$(" div.css-1lda7xt.eqzxucj5 > article");
+  //   expect(cardProject.length).toEqual(3);
+
+  //   console.log("*****");
+  //   console.log(LoginPage.latestProjectListOptions[0].getText());
+  //   expect(await LoginPage.latestProjectListOptions.isDisplayed()).toBeTruthy();
+
+  //   await loginPage.firstProject.click();
+  //   await browser.pause(5000);
+  //   await loginPage.firstProjectCode.click();
+  //   await browser.pause(5000);
+  //   await loginPage.secondProject.click();
+  //   await browser.pause(5000);
+  //   await loginPage.secondProjectCode.click();
+  //   await browser.pause(5000);
+  //   await loginPage.thirdProject.click();
+  //   await browser.pause(5000);
+  //   await loginPage.thirdProjectCode.click();
+  //   await browser.pause(5000);
+  // });
+
+  // it("Posts (from others) You Must Read", async () => {
+  //   const cardProject = await LoginPage.latestProjectList.$$(" div.css-1lda7xt.eqzxucj5 > article");
+  //   expect(cardProject.length).toEqual(3);
+
+  //   console.log("*****");
+  //   console.log(LoginPage.latestProjectListOptions[0].getText());
+  //   expect(await LoginPage.latestProjectListOptions.isDisplayed()).toBeTruthy();
+
+  //   await loginPage.firstProject.click();
+  //   await browser.pause(5000);
+  //   await loginPage.firstProjectCode.click();
+  //   await browser.pause(5000);
+  //   await loginPage.secondProject.click();
+  //   await browser.pause(5000);
+  //   await loginPage.secondProjectCode.click();
+  //   await browser.pause(5000);
+  //   await loginPage.thirdProject.click();
+  //   await browser.pause(5000);
+  //   await loginPage.thirdProjectCode.click();
+  //   await browser.pause(5000);
+  // });
+
+  //
 });
